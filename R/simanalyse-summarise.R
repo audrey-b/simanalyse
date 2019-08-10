c073d646453509cccbcf16a802bce606e8b4022f
audrey-b <audrey.beliveau@uwaterloo.ca>
2019-08-10 00:33:44 -0400

    starting on the summarise function

--- R/simanalyse-summarise.R
+++ R/simanalyse-summarise.R
@@ -6,25 +6,41 @@
 #' R code can be used to customize the summary measures and to coerce results of analyses to
 #' nlist when neccessary.
 #' 
-#' @param measures A vector of strings indicating which Monte Carlo measures to calculate. Strings may include
-#' "bias", "rb" (relative bias), "br" (bias ratio), "var" (variance), "se" (standard error),
-#' "mse" (mean square error), "rmse" (root mean square error), "rrmse" (relative root mean square error), 
-#' "cp" (coverage probability)
-#' @param code A string of R functions to define custom measures.
+#' @param results.nlists An nlists of results
+#' @param measures A vector of strings indicating which Monte Carlo measures to calculate. Strings may include bias, rb (relative 
+#' bias), br (bias ratio), var (variance), se (standard error), mse (mean square error), rmse (root mean square 
+#' error), rrmse (relative root mean square error), cp90, cp95 and cp99 (coverage probability of 90, 95 and 99 percent quantile-based CrIs)
+#' @param estimator A function, typically mean or median, for the Bayes estimator to use.
 #' @param monitor  A character vector (or regular expression if a string) specifying the names of the stochastic nodes in code to include in the summary. By default all stochastic nodes are included.
-#' @param parallel An integer specifying the number of CPU cores to use for generating the datasets in parallel. Defaul is 1 (not parallel).
-#' @param path A string specifying the path to the directory where the results were saved. By default \code{path = NULL } the data sets are not saved but are returned as an nlists object.
-#' @param exists A flag specifying whether the summaries should already exist. If \code{exists = NA} it doesn't matter. If the directory already exists it is overwritten if \code{exists = TRUE} or \code{exists = NA} otherwise an error is thrown.
-#' @param silent A flag specifying whether to suppress warnings.
-
+#' 
 #' @return A flag.
 #' @export
 #'
-#' @examples
-#' simanalyse_summarise()
-#' simanalyse_summarise(FALSE)
-# simanalyse_summarise <- function(x = TRUE) {
-#   check_flag(x)
-#   x
-# }
+
+#need to add example
+#  code A string of R functions to define custom measures.
+#  parallel An integer specifying the number of CPU cores to use for generating the datasets in parallel. Defaul is 1 (not parallel).
+#  path A string specifying the path to the directory where the results were saved. By default \code{path = NULL } the data sets are not saved but are returned as an nlists object.
+#  exists A flag specifying whether the summaries should already exist. If \code{exists = NA} it doesn't matter. If the directory already exists it is overwritten if \code{exists = TRUE} or \code{exists = NA} otherwise an error is thrown.
+#  silent A flag specifying whether to suppress warnings.
+
+simanalyse_summarise <- function(results.nlists, 
+                                 measures, 
+                                 estimator, 
+                                 monitor){
+     
+     results.nlists %<>% subset(select=monitor)
+     
+     if(("var" %in% measures) | ("se" %in% measures)){
+       expr.FUN = function(m) paste0("var <- var(",m,")")
+       expr <- monitor %>% 
+         sapply(expr.FUN) %>% 
+         paste(collapse=" \n ")
+       summ.var <- mcmc_derive(results.nlists, expr=expr) %>% 
+         aggregate(FUN = mean) %>%
+         as.numeric()
+       summ.se <- sqrt(summ.var)
+     }
+}
+
 
