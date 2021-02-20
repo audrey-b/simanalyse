4f654bbc5eac26261319a35ec8d00b11f67eebac
Joe Thorley <joe@poissonconsulting.ca>
2021-02-19 21:08:09 -0800

    tidy up spaces

--- R/sma_set_mode.R
+++ R/sma_set_mode.R
@@ -3,7 +3,7 @@
 #' Set mode for data analysis. 
 #' The algorithm starts with an adaptation phase of `n.adapt` iterations.
 #' Then `n.chains` are ran for `n.save` iterations without thinning. 
-#' The algorithm stops if convergence was achieved according to `r.hat` and 
+#' The algorithm stops if convergence was achieved according to `r.hat` and
 #' the minimum effective sampling rate was achieved or `max.time` was reached or
 #'  is projected to be reached in the next round of iterations. 
 #'  If the algorithm is not stopped, 
@@ -26,25 +26,16 @@
 #' @param max.time A number specifying the maximum time to spend on analysis.
 #' @param units A character string specifying the units of time for `max.time`. 
 #' See `difftime`.
-
 #' The possible modes are as follows:
-
 #' \describe{
-
 #'   \item{'quick'}{To rapidly identify problems with a model definition.}
-
 #'   \item{'report'}{To produce results for a report.}
-
 #'   \item{'paper'}{To produce results for a peer-reviewed paper.}
-
 #' }
-
 #' @export
-
 #' @examples
 #'
 #' \dontrun{
-#'
 #' sma_set_mode("quick")
 #' }
 #'
@@ -90,71 +81,42 @@ sma_set_mode <- function(mode = "report",
 
     list(
       n.chains = ifelse(missing(n.chains), 2L, n.chains),
-
       n.adapt = ifelse(missing(n.adapt), 200L, n.adapt),
-
       ess = ess,
-
       ess.nodes = ifelse(missing(ess.nodes), ".*", ess.nodes),
-
       r.hat = ifelse(missing(r.hat), 1.05, r.hat),
-
       r.hat.nodes = ifelse(missing(r.hat.nodes), ".*", r.hat.nodes),
-
       n.save = ifelse(missing(n.save), 10 * ess, n.save),
-
       max.iter = ifelse(missing(max.iter), 10 * ess, max.iter),
-
       max.time = ifelse(missing(max.time), .Machine$double.xmax, max.time),
-
       units = ifelse(missing(units), "mins", units)
     )
   } else if (mode == "report") {
     ess <- ifelse(missing(ess), 400, ess)
-
     list(
       n.chains = ifelse(missing(n.chains), 3L, n.chains),
-
       n.adapt = ifelse(missing(n.adapt), 5000L, n.adapt),
-
       ess = ess,
-
       ess.nodes = ifelse(missing(ess.nodes), ".*", ess.nodes),
-
       r.hat = ifelse(missing(r.hat), 1.1, r.hat),
-
       r.hat.nodes = ifelse(missing(r.hat.nodes), ".*", r.hat.nodes),
-
       n.save = ifelse(missing(n.save), 10 * ess, n.save),
-
       max.iter = ifelse(missing(max.iter), .Machine$integer.max, max.iter),
-
       max.time = ifelse(missing(max.time), .Machine$double.xmax, max.time),
-
       units = ifelse(missing(units), "mins", units)
     )
   } else if (mode == "paper") {
     ess <- ifelse(missing(ess), 1000, ess)
-
     list(
       n.chains = ifelse(missing(n.chains), 3L, n.chains),
-
       n.adapt = ifelse(missing(n.adapt), 10000L, n.adapt),
-
       ess = ess,
-
       ess.nodes = ifelse(missing(ess.nodes), ".*", ess.nodes),
-
       r.hat = ifelse(missing(r.hat), 1.05, r.hat),
-
       r.hat.nodes = ifelse(missing(r.hat.nodes), ".*", r.hat.nodes),
-
       n.save = ifelse(missing(n.save), 10 * ess, n.save),
-
       max.iter = ifelse(missing(max.iter), .Machine$integer.max, max.iter),
-
       max.time = ifelse(missing(max.time), .Machine$double.xmax, max.time),
-
       units = ifelse(missing(units), "mins", units)
     )
   }
